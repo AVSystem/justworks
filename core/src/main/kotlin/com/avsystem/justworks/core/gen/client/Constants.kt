5b5a4ccc46a093cf6e543f5c0e87560043d9195c
Bartłomiej Kozak <b.kozak@avsystem.com>
2026-03-26 11:45:32 +0100

    refactor: add constants for common content types in `Constants.kt`

---
+++ core/src/main/kotlin/com/avsystem/justworks/core/gen/client/Constants.kt
@@ -0,0 +1,4 @@
+package com.avsystem.justworks.core.gen.client
+
+internal const val MULTIPART_FORM_DATA = "multipart/form-data"
+internal const val FORM_URL_ENCODED = "application/x-www-form-urlencoded"
